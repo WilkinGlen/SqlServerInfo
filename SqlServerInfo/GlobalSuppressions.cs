862b9933e4647650ccd49050931ea3c498c04ada
Glen Wilkin <glen@luthansolutions.com>
2025-03-11 13:19:02 +0000

    Refactor

--- SqlServerInfo/GlobalSuppressions.cs
+++ SqlServerInfo/GlobalSuppressions.cs
@@ -6,3 +6,27 @@
     Justification = "<Pending>", 
     Scope = "member", 
     Target = "~M:SqlServerInfo.Services.SqlServerInfoService.GetDatabasesAsync(System.String)~System.Threading.Tasks.Task{System.Collections.Generic.IEnumerable{SqlServerInfo.Models.DatabaseInfo}}")]
+[assembly: SuppressMessage(
+    "Style",
+    "IDE0058:Expression value is never used",
+    Justification = "<Pending>",
+    Scope = "member",
+    Target = "~M:SqlServerInfo.Services.SqlServerInfoService.PopulateTables(System.String,System.String)~System.Threading.Tasks.Task{System.ValueTuple{System.Collections.Generic.List{SqlServerInfo.Models.TableInfo},Microsoft.Data.SqlClient.SqlConnection}}")]
+[assembly: SuppressMessage(
+    "Style", 
+    "IDE0058:Expression value is never used",
+    Justification = "<Pending>", 
+    Scope = "member", 
+    Target = "~M:SqlServerInfo.Services.SqlServerInfoService.PopulateTable(Microsoft.Data.SqlClient.SqlConnection,System.String,System.Collections.Generic.List{SqlServerInfo.Models.ColumnInfo})~System.Threading.Tasks.Task")]
+[assembly: SuppressMessage(
+    "Style", 
+    "IDE0058:Expression value is never used", 
+    Justification = "<Pending>", 
+    Scope = "member", 
+    Target = "~M:SqlServerInfo.Services.SqlServerInfoService.PopulateKeys(Microsoft.Data.SqlClient.SqlConnection,System.String,System.Collections.Generic.List{SqlServerInfo.Models.KeyInfo})~System.Threading.Tasks.Task")]
+[assembly: SuppressMessage(
+    "Style", 
+    "IDE0058:Expression value is never used", 
+    Justification = "<Pending>", 
+    Scope = "member", 
+    Target = "~M:SqlServerInfo.Services.SqlServerInfoService.PopulateIndexes(Microsoft.Data.SqlClient.SqlConnection,System.String,System.Collections.Generic.List{SqlServerInfo.Models.IndexInfo})~System.Threading.Tasks.Task{System.ValueTuple{Microsoft.Data.SqlClient.SqlCommand,Microsoft.Data.SqlClient.SqlDataReader}}")]
--- SqlServerInfo/Services/SqlServerInfoService.cs
+++ SqlServerInfo/Services/SqlServerInfoService.cs
@@ -28,63 +28,80 @@ public sealed class SqlServerInfoService : ISqlServerInfoService
                 continue;
             }
 
-            var tables = new List<TableInfo>();
-            await using var dbConn = new SqlConnection($"{connectionString}Database={databaseName};");
-            await dbConn.OpenAsync();
-            var tableSchema = await dbConn.GetSchemaAsync("Tables");
-            foreach (DataRow tableRow in tableSchema.Rows)
-            {
-                var tableSchemaName = tableRow["TABLE_SCHEMA"].ToString()!;
-                var tableName = tableRow["TABLE_NAME"].ToString()!;
-                var columns = new List<ColumnInfo>();
-                var keys = new List<KeyInfo>();
-                var indexes = new List<IndexInfo>();
-
-                await using (var cmd = new SqlCommand(SqlServerInfoServiceSqlScripts.GetColumnsSql, dbConn))
-                {
-                    cmd.Parameters.AddWithValue("@tableName", tableName);
-                    await using var reader = await cmd.ExecuteReaderAsync();
-                    while (await reader.ReadAsync())
-                    {
-                        columns.Add(new ColumnInfo(
-                            reader["COLUMN_NAME"].ToString()!,
-                            reader["DATA_TYPE"].ToString()!,
-                            reader["CHARACTER_MAXIMUM_LENGTH"]
-                        ));
-                    }
-                }
-
-                await using (var keyCmd = new SqlCommand(SqlServerInfoServiceSqlScripts.GetKeysSql, dbConn))
-                {
-                    keyCmd.Parameters.AddWithValue("@tableName", tableName);
-                    await using var keyReader = await keyCmd.ExecuteReaderAsync();
-                    while (await keyReader.ReadAsync())
-                    {
-                        keys.Add(new KeyInfo(
-                            keyReader["CONSTRAINT_NAME"].ToString()!,
-                            keyReader["CONSTRAINT_TYPE"].ToString()!,
-                            keyReader["COLUMN_NAME"].ToString()!
-                        ));
-                    }
-                }
-
-                await using var indexCmd = new SqlCommand(SqlServerInfoServiceSqlScripts.GetIndexesSql, dbConn);
-                indexCmd.Parameters.AddWithValue("@tableName", tableName);
-                await using var indexReader = await indexCmd.ExecuteReaderAsync();
-                while (await indexReader.ReadAsync())
-                {
-                    indexes.Add(new IndexInfo(
-                        indexReader["IndexName"].ToString()!,
-                        indexReader["IndexType"].ToString()!
-                    ));
-                }
-
-                tables.Add(new TableInfo(tableSchemaName, tableName, columns, keys, indexes));
-            }
-
+            var (tables, dbConn) = await PopulateTables(connectionString, databaseName);
             databases.Add(new DatabaseInfo(databaseName, tables));
         }
 
         return databases;
+    }
+
+    private static async Task<(List<TableInfo> tables, SqlConnection dbConn)> PopulateTables(string connectionString, string databaseName)
+    {
+        var tables = new List<TableInfo>();
+        await using var dbConn = new SqlConnection($"{connectionString}Database={databaseName};");
+        await dbConn.OpenAsync();
+        var tableSchema = await dbConn.GetSchemaAsync("Tables");
+        foreach (DataRow tableRow in tableSchema.Rows)
+        {
+            var tableSchemaName = tableRow["TABLE_SCHEMA"].ToString()!;
+            var tableName = tableRow["TABLE_NAME"].ToString()!;
+            var columns = new List<ColumnInfo>();
+            var keys = new List<KeyInfo>();
+            var indexes = new List<IndexInfo>();
+
+            await PopulateTable(dbConn, tableName, columns);
+            await PopulateKeys(dbConn, tableName, keys);
+            await PopulateIndexes(dbConn, tableName, indexes);
+
+            tables.Add(new TableInfo(tableSchemaName, tableName, columns, keys, indexes));
+        }
+
+        return (tables, dbConn);
+    }
+
+    private static async Task PopulateTable(SqlConnection dbConn, string tableName, List<ColumnInfo> columns)
+    {
+        await using var cmd = new SqlCommand(SqlServerInfoServiceSqlScripts.GetColumnsSql, dbConn);
+        cmd.Parameters.AddWithValue("@tableName", tableName);
+        await using var reader = await cmd.ExecuteReaderAsync();
+        while (await reader.ReadAsync())
+        {
+            columns.Add(new ColumnInfo(
+                reader["COLUMN_NAME"].ToString()!,
+                reader["DATA_TYPE"].ToString()!,
+                reader["CHARACTER_MAXIMUM_LENGTH"]
+            ));
+        }
+    }
+
+    private static async Task PopulateKeys(SqlConnection dbConn, string tableName, List<KeyInfo> keys)
+    {
+        await using var keyCmd = new SqlCommand(SqlServerInfoServiceSqlScripts.GetKeysSql, dbConn);
+        keyCmd.Parameters.AddWithValue("@tableName", tableName);
+        await using var keyReader = await keyCmd.ExecuteReaderAsync();
+        while (await keyReader.ReadAsync())
+        {
+            keys.Add(new KeyInfo(
+                keyReader["CONSTRAINT_NAME"].ToString()!,
+                keyReader["CONSTRAINT_TYPE"].ToString()!,
+                keyReader["COLUMN_NAME"].ToString()!
+            ));
+        }
+    }
+
+    private static async Task<(SqlCommand indexCmd, SqlDataReader indexReader)> PopulateIndexes(SqlConnection dbConn, string tableName, List<IndexInfo> indexes)
+    {
+        await using var indexCmd = new SqlCommand(SqlServerInfoServiceSqlScripts.GetIndexesSql, dbConn);
+        indexCmd.Parameters.AddWithValue("@tableName", tableName);
+        await using var indexReader = await indexCmd.ExecuteReaderAsync();
+        while (await indexReader.ReadAsync())
+        {
+            indexes.Add(new IndexInfo(
+                indexReader["IndexName"].ToString()!,
+                indexReader["IndexType"].ToString()!
+            ));
+        }
+
+        return (indexCmd, indexReader);
     }
 }
