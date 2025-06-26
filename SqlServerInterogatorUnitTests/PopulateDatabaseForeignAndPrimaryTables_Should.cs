72f0189ecdff09a14b858bbfb6161c0c6ac92c7d
Glen Wilkin <glen@luthansolutions.com>
2025-06-26 11:37:16 +0100

    Add WHERE clauses from parameters

--- SqlServerInterogatorUnitTests/PopulateDatabaseForeignAndPrimaryTables_Should.cs
+++ SqlServerInterogatorUnitTests/PopulateDatabaseForeignAndPrimaryTables_Should.cs
@@ -277,4 +277,119 @@ public class PopulateDatabaseForeignAndPrimaryTables_Should
 
         _ = databaseInfo.Tables.Should().BeEmpty();
     }
+
+    [Fact]
+    public void PopulateTablesICanJoinTo_ShouldHandleMultipleDisconnectedGraphs()
+    {
+        // Two separate groups: (A <-> B), (C <-> D)
+        var databaseInfo = new DatabaseInfo
+        {
+            Name = "TestDatabase",
+            Tables =
+            [
+                new TableInfo { Name = "A", SchemaName = "dbo", TableId = 1 },
+                new TableInfo { Name = "B", SchemaName = "dbo", TableId = 2 },
+                new TableInfo { Name = "C", SchemaName = "dbo", TableId = 3 },
+                new TableInfo { Name = "D", SchemaName = "dbo", TableId = 4 }
+            ]
+        };
+        databaseInfo.Tables[0].Keys.Add(new KeyInfo { IsForeignKey = true, ReferencedTableName = "B" }); // A → B
+        databaseInfo.Tables[1].Keys.Add(new KeyInfo { IsForeignKey = true, ReferencedTableName = "A" }); // B → A
+        databaseInfo.Tables[2].Keys.Add(new KeyInfo { IsForeignKey = true, ReferencedTableName = "D" }); // C → D
+        databaseInfo.Tables[3].Keys.Add(new KeyInfo { IsForeignKey = true, ReferencedTableName = "C" }); // D → C
+
+        DatabaseInterrogator.PopulateDatabaseForeignAndPrimaryTables(databaseInfo);
+
+        // A and B can only join to each other, C and D can only join to each other
+        _ = databaseInfo.Tables[0].TablesICanJoinTo.Should().ContainSingle(t => t.Name == "B");
+        _ = databaseInfo.Tables[1].TablesICanJoinTo.Should().ContainSingle(t => t.Name == "A");
+        _ = databaseInfo.Tables[2].TablesICanJoinTo.Should().ContainSingle(t => t.Name == "D");
+        _ = databaseInfo.Tables[3].TablesICanJoinTo.Should().ContainSingle(t => t.Name == "C");
+    }
+
+    [Fact]
+    public void PopulateTablesICanJoinTo_ShouldHandleTableWithNoKeys()
+    {
+        var databaseInfo = new DatabaseInfo
+        {
+            Name = "TestDatabase",
+            Tables =
+            [
+                new TableInfo { Name = "Lonely", SchemaName = "dbo", TableId = 1 }
+            ]
+        };
+
+        DatabaseInterrogator.PopulateDatabaseForeignAndPrimaryTables(databaseInfo);
+
+        _ = databaseInfo.Tables[0].TablesICanJoinTo.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void PopulateTablesICanJoinTo_ShouldHandleCaseInsensitiveTableNames()
+    {
+        var databaseInfo = new DatabaseInfo
+        {
+            Name = "TestDatabase",
+            Tables =
+            [
+                new TableInfo { Name = "Parent", SchemaName = "dbo", TableId = 1 },
+                new TableInfo { Name = "child", SchemaName = "dbo", TableId = 2 }
+            ]
+        };
+        databaseInfo.Tables[1].Keys.Add(new KeyInfo { IsForeignKey = true, ReferencedTableName = "PARENT" });
+
+        DatabaseInterrogator.PopulateDatabaseForeignAndPrimaryTables(databaseInfo);
+
+        _ = databaseInfo.Tables[1].TablesICanJoinTo.Should().ContainSingle(t => t.Name == "Parent");
+    }
+
+    [Fact]
+    public void PopulateTablesICanJoinTo_ShouldNotAddDuplicateTables_WhenIndirectAndDirectJoinsExist()
+    {
+        var databaseInfo = new DatabaseInfo
+        {
+            Name = "TestDatabase",
+            Tables =
+            [
+                new TableInfo { Name = "A", SchemaName = "dbo", TableId = 1 },
+                new TableInfo { Name = "B", SchemaName = "dbo", TableId = 2 },
+                new TableInfo { Name = "C", SchemaName = "dbo", TableId = 3 }
+            ]
+        };
+        // A → B (direct), A → C (direct), B → C (direct)
+        databaseInfo.Tables[0].Keys.Add(new KeyInfo { IsForeignKey = true, ReferencedTableName = "B" });
+        databaseInfo.Tables[0].Keys.Add(new KeyInfo { IsForeignKey = true, ReferencedTableName = "C" });
+        databaseInfo.Tables[1].Keys.Add(new KeyInfo { IsForeignKey = true, ReferencedTableName = "C" });
+
+        DatabaseInterrogator.PopulateDatabaseForeignAndPrimaryTables(databaseInfo);
+
+        // A can join to B and C, but C should not appear twice
+        _ = databaseInfo.Tables[0].TablesICanJoinTo.Should().Contain(t => t.Name == "B");
+        _ = databaseInfo.Tables[0].TablesICanJoinTo.Should().Contain(t => t.Name == "C");
+        _ = databaseInfo.Tables[0].TablesICanJoinTo.Should().HaveCount(2);
+    }
+
+    [Fact]
+    public void PopulateTablesICanJoinTo_ShouldHandleLargeNumberOfTables()
+    {
+        var tables = new List<TableInfo>();
+        for (var i = 1; i <= 50; i++)
+        {
+            tables.Add(new TableInfo { Name = $"T{i}", SchemaName = "dbo", TableId = i });
+        }
+        // Create a chain: T1 → T2 → T3 ... → T50
+        for (var i = 0; i < 49; i++)
+        {
+            tables[i].Keys.Add(new KeyInfo { IsForeignKey = true, ReferencedTableName = $"T{i + 2}" });
+        }
+
+        var databaseInfo = new DatabaseInfo { Name = "TestDatabase", Tables = tables };
+
+        DatabaseInterrogator.PopulateDatabaseForeignAndPrimaryTables(databaseInfo);
+
+        // T1 should be able to join to all others
+        _ = databaseInfo.Tables[0].TablesICanJoinTo.Should().HaveCount(49);
+        // T50 should be able to join to all others (if bidirectional is correct)
+        _ = databaseInfo.Tables[49].TablesICanJoinTo.Should().HaveCount(49);
+    }
 }
